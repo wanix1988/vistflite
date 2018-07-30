68d4a8b514d90f3e154a953eb41c01e5da4483a2
linwei.wang <linwei.wang@mediatek.com>
2018-07-30 21:28:23 +0800

    Add upload webpage

--- app/main/vistflite.py
+++ app/main/vistflite.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+import os
 import sys
 #sys.path.append('..')
 
 from flask import Flask
 from flask import render_template
+from flask import request, flash, redirect, url_for
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 from tflite.Model import Model
 from tflite.OperatorCode import OperatorCode
 from tflite.BuiltinOperator import BuiltinOperator
 from tflite.SubGraph import SubGraph
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 #1GB
+
+bootstrap = Bootstrap(app)
 
 def getBuiltinOperatorStringName(idx):
     allAttrs = [i for i in BuiltinOperator.__dict__.items() if not i[0].startswith('__')]
@@ -21,9 +29,25 @@ def getBuiltinOperatorStringName(idx):
 
 @app.route('/')
 def index():
-    return render_template('analyze_tflite.html', name='tflite')
+    return render_template(r'analyze_tflite.html', name='tflite')
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] == 'tflite'
+
+@app.route('/analyze_tflite', methods=['POST'])
 def analyze_tflite():
+    if not request.files:
+        flash('TFLite file is required!!!')
+        return redirect(request.url)
+    f = request.files['tflite']
+    print(dir(f))
+    if f and allowed_file(f.filename):
+        fname = secure_filename(f.filename)
+        return fname
+    flash('Wrong Request!!!')
+    return redirect(request.url)
+
+def __analyze_tflite():
     print('vistflite...')
     with open(sys.argv[1], 'rb') as df:
         content = df.read()
@@ -45,6 +69,14 @@ def analyze_tflite():
             print('Tensors:')
             for j in range(subGraph.TensorsLength()):
                 print(subGraph.Tensors(j).Name())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+ 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
