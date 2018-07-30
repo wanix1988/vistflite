a173716f8c989c4683d55917ae01293122e29097
linwei.wang <wanix1988@gmail.com>
2018-07-31 00:12:13 +0800

    add model details table

--- app/main/vistflite.py
+++ app/main/vistflite.py
@@ -3,6 +3,8 @@
 
 import os
 import sys
+import tempfile
+import struct
 #sys.path.append('..')
 
 from flask import Flask
@@ -18,6 +20,9 @@ from tflite.SubGraph import SubGraph
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 #1GB
+app.config['UPLOAD_FOLDER'] = tempfile.gettempdir()
+
+IN_MEMORY_SIZE = 100 * 1024 * 1024 #100MB
 
 bootstrap = Bootstrap(app)
 
@@ -36,6 +41,7 @@ def allowed_file(filename):
 
 @app.route('/analyze_tflite', methods=['POST'])
 def analyze_tflite():
+    global IN_MEMORY_SIZE
     if not request.files:
         flash('TFLite file is required!!!')
         return redirect(request.url)
@@ -43,32 +49,46 @@ def analyze_tflite():
     print(dir(f))
     if f and allowed_file(f.filename):
         fname = secure_filename(f.filename)
-        return fname
+        print(dir(f))
+        f.seek(0, os.SEEK_END)
+        length = f.tell()
+        f.seek(0, os.SEEK_SET)
+        print('length:', length)
+        if length < IN_MEMORY_SIZE:
+            #content = struct.unpack('!c', f.stream.read())
+            content = f.stream.read()
+            return __analyze_tflite(fname, content)
+        else:
+            return f.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
     flash('Wrong Request!!!')
     return redirect(request.url)
 
-def __analyze_tflite():
+def __analyze_tflite(fname, buffer):
     print('vistflite...')
-    with open(sys.argv[1], 'rb') as df:
-        content = df.read()
-        model = Model.GetRootAsModel(content, 0)
-        print(model.Version())
-        print(model.Description())
-        # OperatorCodes
-        for i in range(model.OperatorCodesLength()):
-            op = model.OperatorCodes(i)
-            if op.CustomCode():
-                print('Custom OP:', op.Version(), op.CustomCode())
-            else:
-                print('Builtin OP:', op.Version(), op.BuiltinCode(), getBuiltinOperatorStringName(op.BuiltinCode()))
-        # SubGraphs
-        print('SubGraphs:')
-        for i in range(model.SubgraphsLength()):
-            subGraph = model.Subgraphs(i)
-            print('Name:', subGraph.Name())
-            print('Tensors:')
-            for j in range(subGraph.TensorsLength()):
-                print(subGraph.Tensors(j).Name())
+    model = Model.GetRootAsModel(buffer, 0)
+    # OperatorCodes
+    operatorCodes = []
+    for i in range(model.OperatorCodesLength()):
+        op = model.OperatorCodes(i)
+        if op.CustomCode():
+            print('Custom OP:', op.Version(), op.CustomCode())
+            operatorCodes.append(['Custom OP:', op.Version(), op.CustomCode()])
+        else:
+            print('Builtin OP:', op.Version(), op.BuiltinCode(), getBuiltinOperatorStringName(op.BuiltinCode()))
+            operatorCodes.append(['Builtin OP:', op.Version(), op.BuiltinCode(), getBuiltinOperatorStringName(op.BuiltinCode())])
+    # SubGraphs
+    print('SubGraphs:')
+    for i in range(model.SubgraphsLength()):
+        subGraph = model.Subgraphs(i)
+        print('Name:', subGraph.Name())
+        print('Tensors:')
+        for j in range(subGraph.TensorsLength()):
+            print(subGraph.Tensors(j).Name())
+    return render_template('tflite_model_details.html',
+                            tflite_file_name=fname,
+                            model_version=model.Version(),
+                            model_description=model.Description(),
+                            model_operator_codes=operatorCodes)
 
 @app.errorhandler(404)
 def page_not_found(e):
